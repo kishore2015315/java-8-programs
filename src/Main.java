18c3b36e84e5dac7ff24a6834729cb0b7d97f37a
kishore2015315 <kishore2015315@gmail.com>
2023-03-08 12:43:02 +0530

    file streams csv

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -10,12 +11,12 @@ public class Main {
         list = new ArrayList<Integer>();
         list.add(1);
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
         String s = null;
         String k = String.valueOf(Optional.empty().map(s1 -> s1.hashCode()).orElse(2));
         System.out.println(k);
-        Person a = new Person("kishore",24);
+        Person a = new Person("kishore","A");
         System.out.println(a.toString());
         Map<String,Integer> map = new HashMap<String,Integer>();
         map.put("hai",1);
@@ -25,12 +26,28 @@ public class Main {
 
         });
        datesPoc();
+        File f = new File("src/data.csv");
+        f.createNewFile();
+        FileWriter fileWriter = new FileWriter(f);
+        PrintWriter printWriter = new PrintWriter(fileWriter);
+        printWriter.println("id,name,course");
+        printWriter.flush();
+        FileReader fileReader = new FileReader(f);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String contents="";
+        while((contents=bufferedReader.readLine())!=null){
+            System.out.println(contents);
+            String s1 [] = contents.split(",");
+            Person p = new Person(s1[0],String.valueOf(s1[1]));
+            System.out.println(p);
+        }
+
     }
 
     static class Person {
         private String name;
-        private int age;
-        Person(String name, int age) {
+        private String age;
+        Person(String name, String age) {
             this.name = name;
             this.age = age;
         }
